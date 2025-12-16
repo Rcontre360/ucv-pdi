112881551281209adef8ec3e95157d19aea65c34
Rcontre360 <rcontreraspimentel@gmail.com>
2025-12-16 19:52:12 -0400

    more comments

--- project-1/src/main/kotlin/core/Image.kt
+++ project-1/src/main/kotlin/core/Image.kt
@@ -6,13 +6,18 @@ import kotlin.Int
 import kotlin.math.roundToInt
 import kotlin.math.sqrt
 
+// zoom algorithm type
 enum class ZoomAlgorithm {
     CLOSEST_NEIGHTBOUR,
     LINEAR_INTERPOLATION,
 }
 
+// callback function for some utilities inside image
 typealias PixelProcessor<T> = (x: Int, y: Int, originalColor: Color) -> T
 
+// metadata class. this represent the current image data, even when we zoom the image pixels grow
+// the bitsPerPixel is internally always 24 but we represent how many bits we would use
+// if the image is STORED
 data class Metadata(
     val width: Int,
     val height: Int,
@@ -21,8 +26,15 @@ data class Metadata(
     val format: String = "Unknown"
 )
 
+// image holds an image and applies operations to it. "image" is NEVER modified
+// instead of modifying it, we copy it and modify the copy. Each time you modify
+// the image you are creating a new copy of it.
+// I used this approach since inmutability allows me to assume things about variables
+// and have "less" bugs. In the future this should be mutable for more performance
 class Image(val buff: BufferedImage) {
     val image: BufferedImage = buff
+    // since this image is inmutable all the related variables can be inmutable
+    // histogram is only calculated once, same with all the other fields
     val histogram: Histogram by lazy { Histogram(calculateHistogram()) }
     val isGrayscale = getIsGrayscale()
     val isBinary = getIsBinary()
@@ -33,6 +45,7 @@ class Image(val buff: BufferedImage) {
         bitsPerPixel = if (isBinary) {1} else{ if (isGrayscale){8} else {24} },
     )
 
+    // runs a function over all pixels, readonly
     fun readAllPixels(processor: PixelProcessor<Any>) {
         for (y in 0 until image.height) {
             for (x in 0 until image.width) {
@@ -43,8 +56,9 @@ class Image(val buff: BufferedImage) {
         }
     }
 
+    // returns the tonal curve of the image. The src image is "this" and the target (f(x)) is the "resImg"
     fun getTonalCurve(resImg: Image): Map<Char, IntArray> {
-        val sums = Array(4) { LongArray(256) { 0L } } // 0:R, 1:G, 2:B, 3:L
+        val sums = Array(4) { LongArray(256) { 0L } }
         val counts = Array(4) { IntArray(256) { 0 } }
 
         for (y in 0 until metadata.height) {
@@ -75,6 +89,8 @@ class Image(val buff: BufferedImage) {
                     luts[c][i] = (sums[c][i] / counts[c][i]).toInt()
                 }
             }
+            // its VERY important to INTERPOLATE. Because for some images that lack some colors the poitns are missing
+            // this will lead to a wrong line graph when displaying it
             interpolate(luts[c])
         }
 
@@ -86,54 +102,56 @@ class Image(val buff: BufferedImage) {
         )
     }
 
+    // this function is used to interpolate the LUT table onn the tonal curve. The smaller and more colorful the image,
+    // the better the tonal curve will look like.  The less colors it has the more spikes and weird details it will have
     private fun interpolate(lut: IntArray) {
         var i = 0
         while (i < lut.size) {
             if (lut[i] == -1) {
                 val prevX = i - 1
                 var nextX = i + 1
+                // interpolation is to fill gaps, so we run from the last known point to the next known point
                 while (nextX < lut.size && lut[nextX] == -1) {
                     nextX++
                 }
 
-                if (prevX < 0) { // Gap at the beginning
-                    val nextY = if (nextX < lut.size) lut[nextX] else i
-                    for (j in i until nextX) {
-                        lut[j] = nextY
-                    }
-                    i = nextX
-                } else if (nextX >= lut.size) { // Gap at the end
-                    val prevY = lut[prevX]
-                    for (j in i until lut.size) {
-                        lut[j] = prevY
-                    }
-                    i = lut.size
-                } else { // Gap in the middle
-                    val prevY = lut[prevX]
-                    val nextY = lut[nextX]
-                    for (j in i until nextX) {
-                        val t = (j - prevX).toFloat() / (nextX - prevX)
-                        lut[j] = (prevY * (1 - t) + nextY * t).roundToInt()
-                    }
-                    i = nextX
+                // then we interpolate points in the middle based on their distance
+                val prevY = if (prevX < 0) {lut[nextX]} else {lut[prevX]}
+                val nextY = if (nextX >= lut.size) {lut[prevX]} else {lut[nextX]}
+                for (j in i until nextX) {
+                    // distance based "t"
+                    val t = (j - prevX).toFloat() / (nextX - prevX)
+                    // interpolation
+                    lut[j] = (prevY * (1 - t) + nextY * t).roundToInt()
                 }
+                i = nextX
             } else {
                 i++
             }
         }
     }
 
+    // applies a kernel to the current image and returns its result
+    // this function calculates the window we must send to the kernel and uses the convolute function to get the value
     fun applyKernel(kernel:Kernel):Image{
-        return applyPerPixel { x, y, color ->
+        // for each pixel we do the following. Calculate the window surrounding that pixel, the size of the kernel
+        return applyPerPixel { x, y, _ ->
+            // the matrixes that will hold the image windows
             val imgR = Array(kernel.rows) {FloatArray(kernel.cols)}
             val imgG = Array(kernel.rows) {FloatArray(kernel.cols)}
             val imgB = Array(kernel.rows) {FloatArray(kernel.cols)}
 
             for (i in 0 until kernel.rows) {
                 for (j in 0 until kernel.cols) {
-                    val imageX = (x - kernel.cols / 2 + j).coerceIn(0, image.width - 1)
-                    val imageY = (y - kernel.rows / 2 + i).coerceIn(0, image.height - 1)
-                    val color = Color(image.getRGB(imageX, imageY))
+                    // we calculate the window index
+                    val imageX = (x - kernel.cols / 2 + j)
+                    val imageY = (y - kernel.rows / 2 + i)
+                    // if the window is OUTSIDE the image. We apply some "padding" with black, this wont mess with borders
+                    val color = if (imageX < 0 || imageX >= image.width || imageY < 0 || imageY >= image.height){
+                        Color.BLACK
+                    } else {
+                        Color(image.getRGB(imageX, imageY))
+                    }
 
                     imgR[i][j] = color.red.toFloat()
                     imgG[i][j] = color.green.toFloat()
@@ -141,6 +159,7 @@ class Image(val buff: BufferedImage) {
                 }
             }
 
+            // we convolute with each channel
             val r = kernel.convolute(imgR).roundToInt().coerceIn(0, 255)
             val g = kernel.convolute(imgG).roundToInt().coerceIn(0, 255)
             val b = kernel.convolute(imgB).roundToInt().coerceIn(0, 255)
@@ -149,14 +168,18 @@ class Image(val buff: BufferedImage) {
         }
     }
 
+    // this applies a given border operator (we can even mix them since this receives 2 kernels)
+    // it applies the kernels and calculates the given gradient
     fun applyBorderOperator(kernelX:Kernel,kernelY:Kernel):Image{
         val imageX = applyKernel(kernelX)
         val imageY = applyKernel(kernelY)
 
-        return applyPerPixel { x, y, color ->
+        return applyPerPixel { x, y, _ ->
+            // get both colors from both images
             val cx = Color(imageX.image.getRGB(x, y))
             val cy = Color(imageY.image.getRGB(x, y))
 
+            // sqrt( x^2 + y^2 ) onn each channel
             val r = sqrt((cx.red*cx.red + cy.red * cy.red).toDouble()).toInt().coerceIn(0, 255)
             val g = sqrt((cx.green * cx.green + cy.green * cy.green).toDouble()).toInt().coerceIn(0, 255)
             val b = sqrt((cx.blue * cx.blue + cy.blue * cy.blue).toDouble()).toInt().coerceIn(0, 255)
@@ -165,11 +188,13 @@ class Image(val buff: BufferedImage) {
         }
     }
 
+    // just a simple grayscale function. With a small trick were we add a tint to make the grayscale
+    // if you pick WHITE it will be the same as a normal grayscale
     fun toGrayscale(tint: Color): Image {
         val (tintR, tintG, tintB) = listOf(tint.red / 255.0, tint.green / 255.0, tint.blue / 255.0)
 
         return applyPerPixel { _, _, color ->
-            val gray = (0.2126 * color.red + 0.7152 * color.green + 0.0722 * color.blue).toInt().coerceIn(0, 255)
+            val gray = luminosity(color)
 
             Color(
                 (gray * tintR).toInt().coerceIn(0, 255),
@@ -189,6 +214,7 @@ class Image(val buff: BufferedImage) {
         }
     }
 
+    // simple brightness function. Instead of adding a constant it multiplies by a factor (between 0-2)
     fun changeBrightness(factor: Float): Image {
         return applyPerPixel { _, _, color ->
             Color(
@@ -199,12 +225,16 @@ class Image(val buff: BufferedImage) {
         }
     }
 
+    // changes the image contrast. The method used is stretching the histogram
+    // to pick the min/max limits we allow the user to do it on will. Since most apps allow the user to change the
+    // contrast "level". We use a factor between 0-1. 0.1 would be stretching the histogram from 12.7 to 255-12.7
     fun changeContrast(factor: Float): Image {
         val min = (127 * factor)
         val max = 255 - (127 * factor)
         val newHistogram = histogram.stretch(min.toInt(),max.toInt())
 
         return applyPerPixel { _, _, color ->
+            // we just map using the new histogram
             val rMap = newHistogram[0]!!
             val gMap = newHistogram[1]!!
             val bMap = newHistogram[2]!!
@@ -217,14 +247,21 @@ class Image(val buff: BufferedImage) {
         }
     }
 
+    // makes a thresholding given a set of umbrals. The umbrals is just an array of numbers between 0 and 255,
+    // where the numbers dont repeat..or shouldnt :)
     fun makeThreshold(umbrals: Array<Int>): Image {
         val sortedUmbrals = umbrals.sorted()
 
         fun thresholding(v: Int): Int {
             if (sortedUmbrals.isEmpty()) return v
 
+            // very easy, this just returns the new value of a given pixel
             for (i in sortedUmbrals.indices) {
+                // since umbrals are sorted, we will start with the lowest one until finding the right umbral
                 if (v < sortedUmbrals[i]) {
+                    // which color is this interval? if we split the 0-255 intervals, the first interval obtained will
+                    // always be BLACK, the next one obviously needs to be white, since black again is like having a single interval.
+                    // we keep doing this until reaching the last interval
                     val intensity = if (i % 2 == 0) {0} else {255}
                     return intensity
                 }
@@ -232,6 +269,7 @@ class Image(val buff: BufferedImage) {
             return 255
         }
 
+        // just applying the above function on a single channel and copying that on the rest
         return applyPerPixel { _, _, color ->
             val gray = color.red
             val newGray = thresholding(gray)
@@ -239,7 +277,10 @@ class Image(val buff: BufferedImage) {
         }
     }
 
-    fun rotateStraight(angle: Int): Image {
+    // we rotate the image on a straight angle
+    fun rotateStraight(_angle: Int): Image {
+        // this fix came from the UI, what happens if we use negative angles?
+        val angle = if (_angle < 0) {270 - _angle} else {_angle}
         if (angle % 90 != 0) return this
 
         val w = metadata.width
@@ -248,6 +289,7 @@ class Image(val buff: BufferedImage) {
         val newWidth: Int
         val newHeight: Int
 
+        // we swap sizes depending on the angle
         when (angle) {
             90, 270 -> {
                 newWidth = h
@@ -265,6 +307,7 @@ class Image(val buff: BufferedImage) {
         for (y in 0 until h) {
             for (x in 0 until w) {
                 val pixel = image.getRGB(x, y)
+                // depending on the angle, we set different positions of the new matrix
                 when (angle) {
                     90 -> newImage.setRGB(h - 1 - y, x, pixel)
                     180 -> newImage.setRGB(w - 1 - x, h - 1 - y, pixel)
@@ -276,6 +319,7 @@ class Image(val buff: BufferedImage) {
         return Image(newImage)
     }
 
+    // applies zoom to the image
     fun zoom(factor: Float, algo: ZoomAlgorithm): Image {
         val w = (metadata.width * factor).toInt()
         val h = (metadata.height * factor).toInt()
@@ -293,11 +337,13 @@ class Image(val buff: BufferedImage) {
                         newImg.setRGB(x, y, pixel)
                     }
                     ZoomAlgorithm.LINEAR_INTERPOLATION -> {
+                        // this was implemented given the pdfs sent on classes
                         val a = (x/factor) - (x / factor).toInt()
                         val b = (y/factor) - (y / factor).toInt()
                         val srcX = (x / factor).toInt().coerceIn(0,metadata.width - 2)
                         val srcY = (y / factor).toInt().coerceIn(0,metadata.height - 2)
 
+                        // given the 4 pixel values, it interpolates the value within them
                         fun interpolateChannel(is_v:Int, ds:Int, ir:Int, dr:Int): Int{
                             return ((1-a) * (1-b) * is_v + a*(1-b) * ds + (1-a)*b*ir + a*b*dr).toInt();
                         }
@@ -321,6 +367,7 @@ class Image(val buff: BufferedImage) {
         return Image(newImg)
     }
 
+    // calculates the histogram of the image
     private fun calculateHistogram(): Map<Int, IntArray> {
         val histogram = mutableMapOf<Int, IntArray>()
         val red = IntArray(256)
