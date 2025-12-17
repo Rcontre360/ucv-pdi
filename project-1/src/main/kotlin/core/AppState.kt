5e4e37af91821a00e75589dc393669d84f7becae
Rcontre360 <rcontreraspimentel@gmail.com>
2025-12-16 22:38:46 -0400

    fixed continuity of operations

--- project-1/src/main/kotlin/core/AppState.kt
+++ project-1/src/main/kotlin/core/AppState.kt
@@ -1,29 +1,34 @@
 package org.pdi.core
 
 import java.awt.Color
-import java.awt.Point
 import java.awt.image.BufferedImage
-import org.pdi.io.loadImage
 import java.io.File
-import javax.imageio.ImageIO
 
+// state context. this class holds values that are used for transition between the initial image and the current one
+// how we decided which values should go here? I wanted those that modify the tonal curve and those that dont interfere with the first ones
+// the ones I want to show in the tonal curve are brightness, contrast and negative. The transformations that dont affect these ones are all
+// geometric transformations
 data class StateContext(
     val currentImage: Image? = null,
     val brightness: Float = 0.0f,
     val contrast: Float = 0.0f,
     val rotationApplied: Int = 0,
-    val currentZoomLevelIndex: Int = 9, // Default for 1.0f
+    val currentZoomLevelIndex: Int = 9,
     val isNegative: Boolean = false
 )
 
 class AppState {
+    // initial image. It changes if we apply one way functions
     private var _initialImage: Image? = null
 
+    // context listeners. Used on some parts of the UI. Helps listen for image changes
     private val _contextListeners = mutableListOf<(StateContext) -> Unit>()
 
+    // context variables
     var context: StateContext = StateContext()
         private set
 
+    // zoom related variables
     var zoomAlgorithm: ZoomAlgorithm = ZoomAlgorithm.LINEAR_INTERPOLATION
     val zoomLevels = listOf(0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f)
 
@@ -31,6 +36,9 @@ class AppState {
         return context.currentImage?.isGrayscale?:false
     }
 
+    // returns tonal curve. From the src image to the target
+    // it breaks when we apply geometrical transformations since the current image
+    // will not have a 1-1 relationship on x,y from the initial image to the current one
     fun getTonalCurve(): Map<Char, IntArray>? {
         return _initialImage?.let { initial ->
             context.currentImage?.let { current ->
@@ -39,6 +47,7 @@ class AppState {
         }
     }
 
+    // I wont comment on this self explanatory functions..
     fun getHistogram(): Histogram? {
         return context.currentImage?.histogram
     }
@@ -47,10 +56,12 @@ class AppState {
         return context.currentImage?.image
     }
 
+    // used on the UI to add a function to listen for the context updates
     fun addContextListener(listener: (StateContext) -> Unit) {
         _contextListeners.add(listener)
     }
 
+    // functions below this one call the "update" function with some object defining the update
     fun applyConvolution(kernel: Kernel) {
         update(UpdateType.ConvolutionUpdate(kernel))
     }
@@ -98,7 +109,8 @@ class AppState {
     fun applyThresholding(thresholds: List<Int>) {
         update(UpdateType.ThresholdUpdate(thresholds))
     }
-        
+
+    // check if the context has changed. If so we will do other thigns on he update function..
     private fun isContextDefault(context: StateContext): Boolean {
         val default = StateContext()
         return context.brightness == default.brightness &&
@@ -107,12 +119,20 @@ class AppState {
                 context.currentZoomLevelIndex == default.currentZoomLevelIndex &&
                 context.isNegative == default.isNegative
     }
-        
+
+    // this function takes the initial image and applies those functions that are not one way
+    // this is debatible since an increase in brightness can be one way (loss of info)
+    // so lets say I choose arbitrarily which functions I want to apply here.
     private fun updateContextChanges(context: StateContext): Image? {
         if (_initialImage == null) return null
+        // we apply each operation in an order that makes sense
+        // for example makes sense to first apply negative, if we apply negative after brightness
+        // an image that is negative will appear darker with a brightness increase
+        // we dont know the effects of the order between contrast and brightness so we leave it arbitrarily
+        // geometric transformations go later since these dont affect the previous ones
         var image = _initialImage!!
-        if (context.rotationApplied != 0) {
-            image = image.rotateStraight(context.rotationApplied)
+        if (context.isNegative) {
+            image = image.negative()
         }
         if (context.brightness != 0.0f) {
             image = image.changeBrightness(context.brightness)
@@ -120,31 +140,32 @@ class AppState {
         if (context.contrast != 0.0f) {
             image = image.changeContrast(context.contrast)
         }
-        if (context.isNegative) {
-            image = image.negative()
-        }
         if (context.currentZoomLevelIndex != 9) {
             val factor = zoomLevels[context.currentZoomLevelIndex]
             image = image.zoom(factor, zoomAlgorithm)
         }
+        if (context.rotationApplied != 0) {
+            image = image.rotateStraight(context.rotationApplied)
+        }
         return image
     }
-        
+
+    // update the internal state. Any image modification will go through this function
+    // this is useful since it allows us to change the image in x way when we made y change
+    // this centralized design allows for complex changes too (in the future)
     private fun update(updateType: UpdateType) {
         var newStateContext = context.copy()
         when (updateType) {
             is UpdateType.Clear -> {
                 newStateContext = StateContext(currentImage = _initialImage)
             }
+            // START ---------------
+            // from here we dont update the image, we just update the variables of the context object
             is UpdateType.BrightnessUpdate -> {
                 newStateContext = newStateContext.copy(brightness = updateType.newFactor)
             }
             is UpdateType.ContrastUpdate -> {
                 newStateContext = newStateContext.copy(contrast = updateType.newFactor)
-            }
-            is UpdateType.GrayscaleUpdate -> {
-                _initialImage = _initialImage?.toGrayscale(updateType.tint)
-                newStateContext = StateContext(currentImage = _initialImage)
             }
             is UpdateType.NegativeUpdate -> {
                 newStateContext = newStateContext.copy(isNegative = updateType.isNegative)
@@ -153,13 +174,6 @@ class AppState {
                 newStateContext = newStateContext.copy(
                     rotationApplied = (newStateContext.rotationApplied + updateType.angle) % 360
                 )
-            }
-            is UpdateType.ThresholdUpdate -> {
-                if (!isCurrentImageGrayscale()) {
-                    return
-                }
-                _initialImage = _initialImage?.makeThreshold(updateType.thresholds.toTypedArray())
-                newStateContext = StateContext(currentImage = _initialImage)
             }
             UpdateType.ZoomInUpdate -> {
                 if (newStateContext.currentZoomLevelIndex < zoomLevels.size - 1) {
@@ -175,6 +189,25 @@ class AppState {
                     )
                 }
             }
+            // END -------
+            // all the operations below update _initialImage.
+            // This means that once these operations are done we cant go back
+            // we didnt took in account how this operations modify the tonal curve
+            // thats why we decided to just change the initial image, to show the tonal
+            // curve as it is f(x) = x
+            is UpdateType.GrayscaleUpdate -> {
+                _initialImage = context.currentImage?.toGrayscale(updateType.tint)
+                newStateContext = StateContext(currentImage = _initialImage)
+            }
+            // thresholding (umbralizacion)
+            is UpdateType.ThresholdUpdate -> {
+                if (!isCurrentImageGrayscale()) {
+                    return
+                }
+                _initialImage = context.currentImage?.makeThreshold(updateType.thresholds.toTypedArray())
+                newStateContext = StateContext(currentImage = _initialImage)
+            }
+            // load an image
             is UpdateType.LoadImageUpdate -> {
                 try {
                     val loadedImage = Image(org.pdi.io.loadImage(updateType.file))
@@ -187,19 +220,20 @@ class AppState {
                 }
             }
             is UpdateType.ConvolutionUpdate -> {
-                _initialImage = _initialImage?.applyKernel(updateType.kernel)
+                _initialImage = context.currentImage?.applyKernel(updateType.kernel)
                 newStateContext = StateContext(currentImage = _initialImage)
             }
             is UpdateType.BorderOperation -> {
-                _initialImage = _initialImage?.applyBorderOperator(updateType.kernelX,updateType.kernelY)
+                _initialImage = context.currentImage?.applyBorderOperator(updateType.kernelX,updateType.kernelY)
                 newStateContext = StateContext(currentImage = _initialImage)
             }
         }
+
+        // if the context has changed we apply the context changes
         if (_initialImage != null && !isContextDefault(newStateContext)) {
             newStateContext = newStateContext.copy(currentImage = updateContextChanges(newStateContext))
-        } else if (_initialImage != null && isContextDefault(newStateContext)) {
-            newStateContext = newStateContext.copy(currentImage = _initialImage)
         }
+
         context = newStateContext
         _contextListeners.forEach { it.invoke(context) }
     }
